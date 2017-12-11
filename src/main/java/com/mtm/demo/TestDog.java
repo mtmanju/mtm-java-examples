9903f6d9f1140fa2ae003ddef5b4b46c5789c35b
Manjunath M T <itzmtmanju@gmail.com>
2017-12-11 14:53:26 +0530

    hi

--- src/main/java/com/mtm/demo/TestDog.java
+++ src/main/java/com/mtm/demo/TestDog.java
@@ -5,33 +5,33 @@ import java.util.List;
 
 class Animal1 {
 
-    public void move() {
-        System.out.println("Animals can move");
-    }
+	public void move() {
+		System.out.println("Animals can move");
+	}
 }
 
 class Dog1 extends Animal1 {
 
-    public void move() {
-        System.out.println("Dogs can walk and run");
-    }
+	public void move() {
+		System.out.println("Dogs can walk and run");
+	}
 }
 
 public class TestDog {
 
-    public static void main(String args[]) {
-        Animal1 a = new Animal1(); // Animal reference and object
-        Animal1 b = new Dog1(); // Animal reference but Dog object
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void main(String args[]) {
+		Animal1 a = new Animal1(); // Animal reference and object
+		Animal1 b = new Dog1(); // Animal reference but Dog object
 
-        a.move();// Runs the method in Animal class
-        b.move();// Runs the method in Dog class
+		a.move();// Runs the method in Animal class
+		b.move();// Runs the method in Dog class
 
-        Dog1 d = (Dog1) new Animal1(); // Will throw class cast exception
-        d.move();
-        
-        List list =new ArrayList<>();
-        list.add("String");
-        
+		Dog1 d = (Dog1) new Animal1(); // Will throw class cast exception
+		d.move();
 
-    }
+		List list = new ArrayList<>();
+		list.add("String");
+
+	}
 }
