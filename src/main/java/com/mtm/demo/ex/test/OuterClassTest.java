ef5bca4beaa00d075b4d7b010a66a4f59b6572c7
Manjunath M T <ManjunathMT@fico.com>
2019-03-06 15:04:20 +0530

    fixed compilation issues

--- src/main/java/com/mtm/demo/ex/test/OuterClassTest.java
+++ src/main/java/com/mtm/demo/ex/test/OuterClassTest.java
@@ -2,14 +2,14 @@ package com.mtm.demo.ex.test;
 
 import com.mtm.demo.ex.Airplane;
 
-public class OuterTest {
+public class OuterClassTest {
 
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-        Airplane.BlackBox box = new Airplane().new BlackBox();
-        box.printVariables();
-    }
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Airplane.BlackBox box = new Airplane().new BlackBox();
+		box.printVariables();
+	}
 
 }
