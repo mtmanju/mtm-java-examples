8e30f2f3c683e7863d8d6aab7493ab182b55fe20
Manjunath M T <ManjunathMT@fico.com>
2019-03-08 11:58:10 +0530

    Added FunctionalInterfaceExample.java

---
+++ src/main/java/com/mtm/demo/java8/FunctionalInterfaceExample.java
@@ -0,0 +1,20 @@
+package com.mtm.demo.java8;
+
+/**
+ * @author Manjunath M T
+ *
+ */
+public class FunctionalInterfaceExample {
+
+	public static void main(String[] args) {
+		Calculator calculator = (int x) -> x - x + x;
+		System.out.println(calculator.calculate(52));
+	}
+}
+
+@FunctionalInterface
+interface Calculator {
+
+	int calculate(int x);
+
+}
